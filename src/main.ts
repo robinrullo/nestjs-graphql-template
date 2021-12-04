65e4df5333b0b4548a5222f0c59d9325b3220704
robin.rullo <robin.rullo@uha.fr>
2021-12-04 12:36:23 +0100

    feat(fastify): set fastify as default adapter

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({}),
+  );
 
   const config = app.get<ConfigService>(ConfigService);
   const address = config.get('app.address');
